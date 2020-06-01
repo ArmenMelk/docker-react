e128bcfcf1b60aba8caa37f06673e46f100c791f
ArmenMelk <amelkumyan87@gmail.com>
2020-06-02 00:12:40 +0400

    change text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React I am back
+          Yo Yo I am back
         </a>
       </header>
     </div>
